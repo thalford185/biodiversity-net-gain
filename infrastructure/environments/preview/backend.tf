b4b0f89232a02c11c02eec42758d7297928efefc
thalford185 <76488749+thalford185@users.noreply.github.com>
2025-03-13 09:24:11 -0400

    Add GitHub Action workflow

---
+++ infrastructure/environments/preview/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "gcs" {
+    bucket = "bng--terraform--preview"
+  }
+}
---
+++ infrastructure/environments/preview/provider.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "bng-preview"
+  region  = "europe-west4"
+}
+
+provider "google-beta" {
+  project = "bng-preview"
+  region  = "europe-west4"
+}
